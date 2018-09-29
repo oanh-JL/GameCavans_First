8de6b3280d62652db3e13241641444cd5f41ca27
oanh-JL <32917256+oanh-JL@users.noreply.github.com>
2018-09-29 22:45:03 +0700

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,49 @@
+package base;
+
+import base.renderer.AnimationRenderer;
+import tklibs.SpriteUtils;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Enemy extends GameObject {
+    public static int rand(int min, int max) {
+        try {
+            Random rn = new Random();
+            int range = max - min + 1;
+            int randomNum = min + rn.nextInt(range);
+            return randomNum;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    public Enemy() {
+        ArrayList<BufferedImage> images = new ArrayList<>();
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/0.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/1.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/2.png"));
+        //images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/3.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/4.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/5.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/6.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/7.png"));
+        images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/8.png"));
+        this.renderer = new AnimationRenderer(images);
+        this.position = new Vector2D(rand(0,380),rand(0,580));
+
+    }
+ @Override
+
+    public void run(){
+        if(this.position.x<0 || this.position.y<0) return;
+        else
+        {
+            this.position.x+=rand(-1,0);
+            this.position.y+=rand(-1,0);
+        }
+
+    }
+
+}
